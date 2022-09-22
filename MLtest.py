0280455bd501c4f98c1d7bd8b0b599a6a35757ae
timelessowl <kpunenko@gmail.com>
2022-09-22 09:13:29 +0500

    ML KNN sem

--- MLtest.py
+++ MLtest.py
@@ -1,7 +1,12 @@
 import sys
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import  *
+from sklearn.metrics import mean_squared_error
 
 
 def Pandas_output_init():
@@ -12,33 +17,41 @@ def Pandas_output_init():
     pd.options.display.max_colwidth = None
 
 
-def DrowPlot(data):
-    plt.figure(figsize=(10, 7))
-    plt.title('Absences distribution')
-    data['absences'].hist()
-    plt.xlabel('absences')
-    plt.ylabel('number of students')
+def DrawPlot(x, y, p):
+    plt.scatter(x[:, p], x[:, 0])
+    plt.xlabel("B")
+    plt.ylabel("Crime rate")
+
     plt.show()
 
 
 
 def main():
 
-    Pandas_output_init()
-    data = pd.read_csv("math_students.csv", delimiter= ',')
+    # Pandas_output_init()
+    # data = pd.read_csv("math_students.csv", delimiter= ',')
+    data = load_boston()
+    x, y = data['data'], data['target']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+
+    knn = KNeighborsRegressor(n_neighbors=5, weights='uniform', p=2)
+    knn.fit(x_train, y_train)
+    prediction = knn.predict(x_test)
+    print(mean_squared_error(y_test, prediction))
+
+    grid_searcher = GridSearchCV(KNeighborsRegressor(),
+                                 param_grid={'n_neighbors': range(1, 40, 2),
+                                             'weights': ['uniform', 'distance'],
+                                             'p': [1, 2, 3]},
+                                 cv=5)
+    grid_searcher.fit(x_train, y_train)
+    best_predict = grid_searcher.predict(x_test)
+    print(mean_squared_error(y_test, best_predict))
 
 
 
-    # data['alc'] = (5 * data['Dalc'] + 2 * data['Walc']) / 7
-    # print(data['activities'].mode())
-    # DrowPlot(data)
-    # print("Сред оценка хикк", data[data['romantic'] == 'no']['G3'].mean())
-    # print("Сред оценка пар", data[data['romantic'] == 'yes']['G3'].mean())
-    # print("их разность", data[data['romantic'] == 'no']['G3'].mean() - data[data['romantic'] == 'yes']['G3'].mean())
-    # mostcommon = data['activities'].value_counts().idxmax()
-    # print(data[data['activities'] == mostcommon]['absences'].value_counts())
-    # print(mostcommon)
-    print(data[data['school'] == 'MS']['age'].min())
 
 
 
