5c193ef2af7403fe2f5e319cd13561ab9c2b3251
timelessowl <kpunenko@gmail.com>
2022-10-01 09:55:58 +0500

    ML_prac 01.10

--- MLtest.py
+++ MLtest.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston, make_moons
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, cross_validate
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -34,39 +34,49 @@ def KnnPredictSqErr(x_train, y_train, x_test, y_test, n, weights, p):
 
 def LinearPredictSqErr(type, x_train, y_train, x_test, y_test):
     if type == "Ridge":
-        linear = Ridge()
-    elif type =="Lasso":
-        linear = Lasso()
+        linear = Ridge(alpha=0.1)
+    elif type == "Lasso":
+        linear = Lasso(alpha=0.1)
     elif type == "Linear":
         linear = LinearRegression()
 
     linear.fit(x_train, y_train)
-    pred = linear.predict(x_test)
-    return mean_squared_error(y_test, pred)
+    prediction = linear.predict(x_test)
+    return mean_squared_error(y_test, prediction), mean_absolute_error(y_test, prediction), prediction[2999]
 
 
 def main():
+    Pandas_output_init()
+    data_train = pd.read_csv("california_housing_train.csv")
+    data_test = pd.read_csv("california_housing_test.csv")
+    x_train, x_test = data_train.drop(['median_house_value'], axis=1), data_test.drop(["median_house_value"], axis=1)
+    y_train, y_test = data_train["median_house_value"], data_test['median_house_value']
+    print(y_test.tail(5))
+    # print("X_TRAIN:\n\r", x_train.head(10))
+    # print("Y_TRAIN:\n\r", y_train.head(10))
+    # print("X_TEST:\n\r", x_test.head(10))
+    # print("Y_TEST:\n\r", y_test.head(10))
+    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-    data = load_boston()
-    x, y = data['data'], data['target']
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
-    # print(mean_absolute_error(y, pred))
+    # knn = KNeighborsRegressor(n_neighbors=5, weights='uniform', p=2)
+    #
     # grid_searcher = GridSearchCV(KNeighborsRegressor(),
-    #                              param_grid={'n_neighbors': range(1, 40, 2),
+    #                              param_grid={'n_neighbors': range(10, 1000, 10),
     #                                          'weights': ['uniform', 'distance'],
     #                                          'p': [1, 2, 3]},
     #                              cv=5)
     # grid_searcher.fit(x_train, y_train)
     # print(grid_searcher.best_params_)
-    # grid_searcher.fit(x, y)
-    # print(grid_searcher.best_params_)
-    # best_pred = grid_searcher.predict(x)
-    # print(mean_squared_error(y, best_pred))
-    print(KnnPredictSqErr(x_train, y_train, x_test, y_test, 5, 'uniform', 2))
-    print(LinearPredictSqErr("Linear", x_train, y_train, x_test, y_test))
-    print(LinearPredictSqErr("Ridge", x_train, y_train, x_test, y_test))
-    print(LinearPredictSqErr("Lasso", x_train, y_train, x_test, y_test))
+    # # grid_searcher.fit(x, y)
+    # # print(grid_searcher.best_params_)
+    # best_pred = grid_searcher.predict(x_test)
+    # print(mean_squared_error(y_test, best_pred))
+
+    # print(KnnPredictSqErr(x_train, y_train, x_test, y_test, 5, 'uniform', 2))
+    print("Linear SqErr:", LinearPredictSqErr("Linear", x_train, y_train, x_test, y_test))
+    print("Ridge SqErr:", LinearPredictSqErr("Ridge", x_train, y_train, x_test, y_test))
+    print("Lasso SqErr:", LinearPredictSqErr("Lasso", x_train, y_train, x_test, y_test))
+    # print(cross_validate(knn, x, y, cv=5))
 
 
 if __name__ == "__main__":
