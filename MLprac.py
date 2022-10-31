02ca9ecbab891fcfd1e63aee6f5bb29f0ce6cd1c
timelessowl <kpunenko@gmail.com>
2022-10-31 09:38:04 +0500

    prac result

--- MLprac.py
+++ MLprac.py
@@ -1,12 +1,33 @@
 import numpy as np
-from sklearn.datasets import load_boston, make_moons
-from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score
+
+
+def Pandas_output_init():
+
+    # sys.stdout = open("result", 'a')
+    pd.options.display.expand_frame_repr = False
+    pd.options.display.max_rows = None
+    pd.options.display.max_columns = None
+    pd.options.display.max_colwidth = None
+
 
 def main():
-    x, y = make_moons(n_samples=1000, noise=0.5, random_state=10)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
 
+    Pandas_output_init()
+
+    data = pd.read_csv("heart.csv")
+    x_train, x_test, y_train, y_test = train_test_split(data.drop('target', axis=1), data['target'], test_size=0.25, random_state=13)
+    # clf = SGDClassifier( max_iter=1000, learning_rate="constant", random_state=13,eta0=0.1, alpha=0, loss="log")
+    clf = SGDClassifier(penalty="l1", max_iter=1000, learning_rate="optimal", random_state=13, alpha=0.1, loss="log")
+    clf.fit(x_train, y_train)
+    pred = clf.predict_proba(x_test)
+    print(data.columns)
+    print(clf.coef_)
+    # print(accuracy_score(y_test, pred))
+    # print(np.sqrt(sum(np.square(clf.coef_[0][:]))))
 
 
 if __name__ == "__main__":
